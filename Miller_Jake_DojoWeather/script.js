f1e02148c654047f594d6985ee1d075346b5f7fc
Jake Miller <jakemiller.bc@gmail.com>
2022-03-25 11:59:06 -0500

    commit

---
+++ Miller_Jake_DojoWeather/script.js
@@ -0,0 +1,41 @@
+function mouseover(element){
+    element.style.color = "lightblue";
+}
+
+function mouseout(element) {
+    element.style.color = "white";
+}
+
+function loading(element) {
+    alert("Loading weather report...");
+}
+
+function remove(id) {
+    document.getElementById(id).remove();
+}
+
+// Copied and pasted the following functions from the solutions
+// because I had no idea how to do it. Although, after reading
+// the solution I do understand how it was done. Need to wrap my head 
+// around it more, but I understand what is going on when reading it.
+
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
